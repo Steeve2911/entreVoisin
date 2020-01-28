28baaf03b5ed2f410a9d102f05f776354eb51c24
Steeve <blad_pa@hotmail.com>
2020-01-28 17:14:18 +0100

    création du floting btn favory

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilNeighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilNeighbour.java
@@ -1,14 +1,29 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.openclassrooms.entrevoisins.R;
 
 
+
+
 public class ProfilNeighbour extends AppCompatActivity {
+
+   private ImageView mActivityImgProfil;
+   private FloatingActionButton mActivityFav;
+   private TextView mActivityName;
+   private ImageView mActivityPhone;
+   private ImageView mActivityLocation;
+   private ImageView mActivityLanguage;
+   private TextView mActivityPrésentation;
+
+
 
 
 
@@ -17,6 +32,13 @@ public class ProfilNeighbour extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorit_neighbour2);
         this.ConfigureToolBar();
+        mActivityImgProfil = (ImageView)findViewById(R.id.activity_image_profil);
+        mActivityFav = (FloatingActionButton) findViewById(R.id.activity_fav);
+        mActivityName = (TextView) findViewById(R.id.activity_name);
+        mActivityPhone = (ImageView) findViewById(R.id.activity_phone);
+        mActivityLocation = (ImageView) findViewById(R.id.activity_location);
+        mActivityLanguage = (ImageView) findViewById(R.id.activity_language);
+        mActivityPrésentation = (TextView) findViewById(R.id.activity_presentation_txt);
 
     }
     private void ConfigureToolBar() {
